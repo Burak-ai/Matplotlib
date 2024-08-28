e9fa5b8d60efd166b54bf6343c7db8453eda5093
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-08-28 22:57:26 +0300

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -68,7 +68,7 @@ plt.plot(t2, np.cos(2*np.pi*t2), " r^")
 plt.show()"""
 
 
-
+"""
 mu, sigma = 100, 15
 x = mu + sigma * np.random.randn(10000)
 
@@ -80,4 +80,30 @@ plt.title("Histogram of IQ")
 plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([40, 160, 0, 0.03])
 plt.grid(True)
+plt.show()"""
+
+
+ 
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+"""
+plt.plot(x, y, color="red", linewidth=2, linestyle="--")
+plt.scatter(x, y, color="blue", marker="o", s=5)
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.title("Sine Wave")
+plt.grid(True)
+plt.show()"""
+
+#      Subplots
+fig, axs = plt.subplots(3, 2)
+axs[0, 0].plot(x, y, color="red", linestyle="-.")
+axs[0, 1].scatter(x, y, color="magenta", marker="4")
+axs[1, 0].bar(x, y, color="indigo")
+axs[1, 1].hist(y, edgecolor="black", facecolor="blue")
+axs[2, 0].boxplot(y)
+axs[2, 1].pie(x, y)
+# .contourf(x, y, z)
+# .axes(projection='3d')
 plt.show()
+
