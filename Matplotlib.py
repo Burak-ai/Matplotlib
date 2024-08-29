4544647b98aa389d6b2000d5f91dccd41746b307
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2024-08-30 00:08:34 +0300

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 plt.ylabel("Some Numbers")
 plt.show()"""
 
+
+
 """
 plt.plot([1, 2, 3, 4], [1, 4, 9, 16], "ro")
 plt.axis((0, 6, 0, 20))
 plt.show()"""
-
 
 
 """"
