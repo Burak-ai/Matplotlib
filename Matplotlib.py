73438a20796a7732441baaff8971550c5e9d086d
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-07-31 21:43:51 +0300

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#             x              y
+#                 x             y
 """plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
 plt.ylabel("Some Numbers")
 plt.show()"""
